4d61c9a4809e62e0d486da87dc0a269f57ed9a15
Olumuyiwa Aro <43145915+muyiwer@users.noreply.github.com>
2023-06-25 18:56:15 +0100

    update

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,14 +1,17 @@
-import { Dashboard } from './components/pages/dashboard'
-import { Login } from './components/pages/login'
-import  './sass/main.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./components/pages/dashboard";
+import { Login } from "./components/pages/login";
+import "./sass/main.scss";
 
 function App() {
-
   return (
-    <>
-     <Dashboard/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
